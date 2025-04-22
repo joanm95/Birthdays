4a1d2a6315069cccde3aa70af7e2abc90070b171
Joan <joanm95@gmail.com>
2025-04-21 23:22:26 -0400

    Step 2: Add a Class to Represent a Friend

---
+++ Friend.swift
@@ -0,0 +1,18 @@
+//
+//  Friend.swift
+//  Birthdays
+//
+//  Created by joan on 4/21/25.
+//
+
+import Foundation
+
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+}
