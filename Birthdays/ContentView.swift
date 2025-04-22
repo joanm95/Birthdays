b3c6c5ffe3ee8f1dbd8a78e8eaf0721d67173260
Joan <joanm95@gmail.com>
2025-04-21 23:42:25 -0400

    Step 3: Create UI for Adding a Friend

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -13,6 +13,8 @@ struct ContentView: View {
         Friend(name: "Elton Lin", birthday: .now),
         Friend(name: "Jenny Court", birthday: Date(timeIntervalSince1970: 0))
     ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
     
     var body: some View {
         NavigationStack {
@@ -24,6 +26,28 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+
+
+                        newName = ""
+                        newBirthday = .now
+                    }
+                    .bold()
+                }
+                .padding()
+                .background(.bar)
+            }
+            
         }
     }
     
